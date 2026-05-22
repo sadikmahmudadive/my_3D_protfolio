b0d26129f5c67061f55d05d4d23b61b1ee993646
Md. Sadik Mahmud Adive <siradive137@gmail.com>
2026-05-23 04:32:18 +0600

    feat: update Projects page with project descriptions, links, and CTA component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import { projects } from "../constants"
+import { arrow } from "../assets/icons";
+import CTA from "../components/CTA";
 
 export const Projects = () => {
   return (
@@ -22,9 +25,25 @@ export const Projects = () => {
                 <img src={project.iconUrl} alt={project.name} className="w-10 h-10 object-contain" />
               </div>
             </div>
+            <div className="mt--5 flex flex-col">
+              <h4 className="text-2xl font-poppins text-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link to={project.link} target="_blank" rel="noopener noreferrer" className="font-semibold  text-blue-600 ">
+                  View Project
+                </Link>
+                <img src={arrow} alt="arrow" className="w-4 h-4 object-contain"/>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+
+      <hr className="border-slate-200"/>
+
+      <CTA />
     </section>
   )
 }
